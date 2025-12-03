3b873d35c07e3d81a373c5d1cb0999c1e471e387
AldairGonzalez <aldair990811@gmail.com>
2025-12-02 19:39:28 -0500

    The first data scripts were created

---
+++ AuthOn_DataBase/Scripts/01_EmailStatus.sql
@@ -0,0 +1,17 @@
+﻿SET IDENTITY_INSERT [dbo].[EmailStatus] ON;
+
+MERGE INTO [dbo].[EmailStatus] AS Target
+USING (VALUES
+    (1, 'PENDING'),
+    (2, 'SENT'),
+    (3, 'FAILED')
+) AS Source ([EmailStatusId], [Code])
+ON (Target.[EmailStatusId] = Source.[EmailStatusId])
+
+WHEN MATCHED THEN
+    UPDATE SET Target.[Code] = Source.[Code]
+
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT ([EmailStatusId], [Code]) VALUES (Source.[EmailStatusId], Source.[Code]);
+
+SET IDENTITY_INSERT [dbo].[EmailStatus] OFF;
---
+++ AuthOn_DataBase/Scripts/04_TypeAction.sql
@@ -0,0 +1,15 @@
+﻿SET IDENTITY_INSERT [dbo].[TypeAction] ON;
+
+MERGE INTO [dbo].[TypeAction] AS Target
+USING (VALUES
+    (4, 'USER_REGISTERED')
+) AS Source ([TypeActionId], [Code])
+ON (Target.[TypeActionId] = Source.[TypeActionId])
+
+WHEN MATCHED THEN
+    UPDATE SET Target.[Code] = Source.[Code]
+
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT ([TypeActionId], [Code]) VALUES (Source.[TypeActionId], Source.[Code]);
+
+SET IDENTITY_INSERT [dbo].[TypeAction] OFF;
